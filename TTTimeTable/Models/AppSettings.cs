c9de2777cc3e4cef82c7ba48cc643e724d67ead1
Askar <axmetov.ackap@rambler.ru>
2019-10-26 10:33:28 +0300

    init

---
+++ TTTimeTable/Models/AppSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TTTimeTable.Models
+{
+    public static class AppSettings
+    {
+        public static string Url { get; set; } = "https://api.telegram.org/bot";
+        public static string Name { get; set; } = "YourTimingBot";
+        public static string Key { get; set; } = "934526732:AAEjmyIIQWuOISQZc8d3ZiXXp8dR1rtCySc";
+    }
+}
